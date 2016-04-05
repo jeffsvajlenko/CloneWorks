da55b236c86eb8d0551b2be6c05789756a23a938
Jeffrey Svajlenko <jeff.svajlenko@gmail.com>
2016-04-04 23:20:38 -0600

    Removed warnings from Stemmer.
    Added converter to full-independent output.

--- src/util/Stemmer.java
+++ src/util/Stemmer.java
@@ -1,6 +1,5 @@
 package util;
-//       http://tartarus.org/martin/PorterStemmer/java.txt
-//       C.J. van Rijsbergen, S.E. Robertson and M.F. Porter, 1980. New models in probabilistic information retrieval. London: British Library. (British Library Research and Development Report, no. 5587).
+//       http://tartarus.org/martin/PorterStemmer/java.txt//       C.J. van Rijsbergen, S.E. Robertson and M.F. Porter, 1980. New models in probabilistic information retrieval. London: British Library. (British Library Research and Development Report, no. 5587).
 //       M.F. Porter, 1980, An algorithm for suffix stripping, Program, 14(3) pp 130−137.
 
 
@@ -38,7 +37,6 @@ package util;
 
 */
 
-import java.io.*;
 
 /**
   * Stemmer, implementing the Porter Stemming Algorithm
@@ -362,70 +360,5 @@ public class Stemmer
    {  k = i - 1;
       if (k > 1) { step1(); step2(); step3(); step4(); step5(); step6(); }
       i_end = k+1; i = 0;
-   }
-
-   /** Test program for demonstrating the Stemmer.  It reads text from a
-    * a list of files, stems each word, and writes the result to standard
-    * output. Note that the word stemmed is expected to be in lower case:
-    * forcing lower case must be done outside the Stemmer class.
-    * Usage: Stemmer file-name file-name ...
-    */
-   public static void main(String[] args)
-   {
-      char[] w = new char[501];
-      Stemmer s = new Stemmer();
-      for (int i = 0; i < args.length; i++)
-      try
-      {
-         FileInputStream in = new FileInputStream(args[i]);
-
-         try
-         { while(true)
-
-           {  int ch = in.read();
-              if (Character.isLetter((char) ch))
-              {
-                 int j = 0;
-                 while(true)
-                 {  ch = Character.toLowerCase((char) ch);
-                    w[j] = (char) ch;
-                    if (j < 500) j++;
-                    ch = in.read();
-                    if (!Character.isLetter((char) ch))
-                    {
-                       /* to test add(char ch) */
-                       for (int c = 0; c < j; c++) s.add(w[c]);
-
-                       /* or, to test add(char[] w, int j) */
-                       /* s.add(w, j); */
-
-                       s.stem();
-                       {  String u;
-
-                          /* and now, to test toString() : */
-                          u = s.toString();
-
-                          /* to test getResultBuffer(), getResultLength() : */
-                          /* u = new String(s.getResultBuffer(), 0, s.getResultLength()); */
-
-                          System.out.print(u);
-                       }
-                       break;
-                    }
-                 }
-              }
-              if (ch < 0) break;
-              System.out.print((char)ch);
-           }
-         }
-         catch (IOException e)
-         {  System.out.println("error reading " + args[i]);
-            break;
-         }
-      }
-      catch (FileNotFoundException e)
-      {  System.out.println("file " + args[i] + " not found");
-         break;
-      }
    }
 }
